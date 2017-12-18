94442addd9ac8da2400407760cb9c98a52a1e998
Carlos Chang <chang.abin@gmail.com>
2017-12-18 17:00:05 +0800

    Boilerplate

--- public/imagebase.js
+++ public/imagebase.js
@@ -3,12 +3,23 @@ var methodOverride = require('method-override')
 require('express-group-routes')
 var app = express()
 
+var path = require('path')
+var cookieParser = require('cookie-parser')
+var session = require('express-session')
+var flash = require('req-flash');
+var RedisStore = require('connect-redis')(session)
+var bodyParser = require('body-parser')
+var csrf = require('csurf')
+var helmet = require('helmet')
+
+var CONFIG = require('../app/config/global.js')
+
 // ========== Settings ========== //
 
-//process.env.NODE_ENV = CONFIG.appenv.env
-//app.set('view engine', 'pug')
-//app.set('views', CONFIG.path.views)
-app.set('port', process.env.PORT || 3001)
+process.env.NODE_ENV = CONFIG.appenv.env
+app.set('view engine', 'pug')
+app.set('views', CONFIG.path.views)
+app.set('port', process.env.PORT || CONFIG.appenv.port)
 
 
 app.get('/', function (req, res) {
