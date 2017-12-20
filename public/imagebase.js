2e3d2b1d1c25d94f27ed7f15b43a3343b62823ae
Carlos Chang <chang.abin@gmail.com>
2017-12-20 13:46:03 +0800

    api-ajax for account-token-auth test

--- public/imagebase.js
+++ public/imagebase.js
@@ -21,11 +21,61 @@ app.set('view engine', 'pug')
 app.set('views', CONFIG.path.views)
 app.set('port', process.env.PORT || CONFIG.appenv.port)
 
+// ========== Global Middlewares ========== //
+app.use(express.static(CONFIG.path.public))
+app.use(helmet())
+app.use(cookieParser())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(methodOverride(function (req, res) {
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    // look in urlencoded POST bodies and delete it
+    var method = req.body._method;
+    delete req.body._method;
+    return method;
+  }
+}))
+/*app.use(csrf({ cookie: true }))
+app.use(session({
+  key: CONFIG.appenv.sessionCookieName,
+  secret: CONFIG.appenv.cookieSessionSecret,
+  store: new RedisStore({
+    host: CONFIG.appenv.redis.host,
+    port: CONFIG.appenv.redis.port,
+    pass: CONFIG.appenv.redis.password
+  }),
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(flash({ locals: 'flash' }))*/
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
+  var soap = require('soap');
+  var url = 'http://notes.carlos-studio.com/soap/demo.wsdl';
+  var args = {"ibm": ""};
+
+  soap.createClient(url, function(err, client) {
+    console.log(client);
+    //console.log(client.getWeatherAsync('ibm'))
+    client.getWeather({name: 'ibm'}, function(err, result, raw, soapHeader) {
+      // result is a javascript object
+      // raw is the raw response
+      // soapHeader is the response soap header as a javascript object
+      console.log(result.value.$value);
+    })
+    /*client.getWeather(args, function(err, result) {
+      console.log(err);
+      //console.log(result);
+      //res.send('Hello World!22')
+    });*/
+  });
 })
 
+// ========== Routes ========== //
+//require(CONFIG.path.routes + '/web')(app)
+require(CONFIG.path.routes + '/api-ajax')(app)
+
+// ========== Listen ========== //
 app.listen(app.get('port'), function(){
   console.log(app.get('env') + ': http://localhost:' + app.get('port'));
 })
---
+++ routes/api-ajax.js
@@ -0,0 +1,17 @@
+var CONFIG = require('../app/config/global.js')
+
+module.exports = function(app){
+
+  var options = {}
+
+  app.group('/api-ajax/v1.0', (app) => {
+    app.post('/account-token-auth', function(req, res){
+      let token = req.body.token
+      if(token == 'abcde'){
+        res.send("1")
+      }
+      res.send("2")
+    })
+  })
+
+}
