6ad76a7ed4068cd26e756b2f5b8893b09aadeac7
Carlos Chang <chang.abin@gmail.com>
2018-02-19 22:49:17 +0800

    account info

--- app/controllers/admin/account.js
+++ app/controllers/admin/account.js
@@ -5,7 +5,26 @@ var organizationModel = require(CONFIG.path.models + '/organization.js')
 
 exports.index = function(options) {
   return function(req, res) {
-    res.render('admin/pages/account')
+    userModel.getOne('u_id', req.session.u_id, function(results){
+      // 取得 organ_id 對應的 organ_name
+      var sort_obj_for_organ = { "column": "created_at", "sort_type": "DESC" }
+      organizationModel.getAll(sort_obj_for_organ, function(results_organ){
+        results.forEach(function(element, index, arr) {
+          // arr 是原來的 results 陣列
+          results_organ.forEach(function(organ, i, arra_organ){
+            if(element.organ_id == organ.organ_id){
+              results[index].organ_name = organ.organ_name
+            }
+          })
+        })
+        //console.log(results)
+        //res.render('admin/management/all_members', {members: results})
+        res.render('admin/pages/account', {account: results[0]})
+      })
+
+
+    })
+
   }
 }
 
