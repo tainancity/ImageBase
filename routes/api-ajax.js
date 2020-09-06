19366bbc0045c6434a405d54211f7a13ea019c4c
Carlos Chang <chang.abin@gmail.com>
2020-09-06 20:00:25 +0800

    update

--- routes/api-ajax.js
+++ routes/api-ajax.js
@@ -547,7 +547,7 @@ module.exports = function(app){
 
     // 圖片下載
     app.post('/images/download', function(req, res){
-
+      console.log("測試")
       //console.log(req.body.files)
       //console.log(CONFIG.path.storage_temp)
 
