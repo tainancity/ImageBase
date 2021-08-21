e6be121408988d05d86d8e2b3c17641dfbabd8fe
Carlos Chang <chang.abin@gmail.com>
2021-08-21 19:56:22 +0800

    update encrypt

--- app/helpers/static.js
+++ app/helpers/static.js
@@ -12,7 +12,15 @@ module.exports = {
   // decrypt text
   decrypt: function(text) {
 
-    return text;
+    const key = crypto.scryptSync(CONFIG.appenv.cipher.password, 'salt', 24);
+    const iv = Buffer.alloc(16, 0); // Initialization vector.
+
+    const decipher = crypto.createDecipheriv(CONFIG.appenv.cipher.algorithm, key, iv);
+
+    let decrypted = decipher.update(text, 'hex', 'utf8');
+    decrypted += decipher.final('utf8');
+    //console.log(`解密後的資料：${decrypted}`);
+    return decrypted;
 
   },
   decrypt_new: function(text) {
