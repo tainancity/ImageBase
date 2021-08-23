8bbd8a49b448b19a8cc1f515969252cc3aa63d84
Carlos Chang <chang.abin@gmail.com>
2021-08-23 20:02:51 +0800

    test

--- for_test/test.js
+++ for_test/test.js
@@ -3,27 +3,24 @@ let CONFIG = require('../app/config/global.js')
 let client_ssh = require('ssh2-sftp-client')
 let client_ssh_sftp = new client_ssh()
 
+let test_v = "here";
 client_ssh_sftp.connect({
   host: CONFIG.appenv.storage.scp.ip,
   port: 22,
   username: CONFIG.appenv.storage.scp.user,
   password: CONFIG.appenv.storage.scp.password
 }).then(() => {
+  console.log(test_v);
   let remoteDir = CONFIG.path.project + "/for_test/abc/a";
-
-  let remoteDir2 = CONFIG.path.project + "/for_test/def/b";
-  client_ssh_sftp.mkdir(remoteDir2, true);
-
-  client_ssh_sftp.mkdir(remoteDir, true);
-  return;
+  return client_ssh_sftp.mkdir(remoteDir, true);
 }).then(() => {
-  // DSC05910.jpg
+  console.log(test_v);
   let localFile = CONFIG.path.project + "/for_test/DSC05910.jpg";
   let remoteFile = CONFIG.path.project + "/for_test/abc/a/DSC05910.jpg";
-  client_ssh_sftp.fastPut(localFile, remoteFile);
-  return;
+  return client_ssh_sftp.fastPut(localFile, remoteFile);
 }).then(() => {
   console.log("執行到這end");
+  console.log(test_v);
   return client_ssh_sftp.end();
 }).catch(err => {
   console.log("這裡");
